a71c41de2196104981b81bc618794100d745d6aa
iamenochchirima <iamenochchirima@gmail.com>
2023-05-11 11:41:35 +0200

    started day 3

---
+++ students_wall/src/students_wall_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { students_wall_backend } from "../../declarations/students_wall_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await students_wall_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
